45d011fab3658b909dbebdbc7e73f027ed4bf37d
Lantianyou <dtlantianyou@gmail.com>
2021-01-07 10:14:47 +0800

    useMemo demo

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,13 @@
+import Link from 'next/link'
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Link href="/memo">
+        React useMemo demo hook
+      </Link>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
