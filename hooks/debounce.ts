46005abab460ab56da6418a1cfdefa14c516fd01
Lantianyou <dtlantianyou@gmail.com>
2021-01-07 10:12:39 +0800

    useMemo demo

---
+++ hooks/debounce.ts
@@ -0,0 +1,8 @@
+const debounce = (fn, wait) => {
+  let timeout;
+
+  return function executeFunction(...args) {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => fn(...args), wait);
+  };
+};
---
+++ pages/memo.tsx
@@ -0,0 +1,39 @@
+import { useState, useMemo } from "react";
+
+function App() {
+  const [count, setCount] = useState(40);
+  const [n, setN] = useState(40);
+
+  const fib = (n) => {
+    if (n === 0 || n === 1) {
+      return 1;
+    } 
+    return fib(n - 1) + fib(n - 2);;
+  };
+
+  const expensiveResult = useMemo(() => fib(n), [n]);
+
+  return (
+    <div className="App">
+      <div>
+        <h2> 普通组件 (fast) </h2>
+        <div> {count} </div>
+        <button onClick={() => setCount((c) => c + 1)}>increment</button>
+      </div>
+      <h2> 复杂组件 (slow 🐌)</h2>
+      <p>
+        fib({n}) = {expensiveResult}
+      </p>
+      <button
+        onClick={() => {
+          setN(n => n + 1);
+        }}
+      >
+        Next word
+      </button>
+      <div></div>
+    </div>
+  );
+}
+
+export default App;
